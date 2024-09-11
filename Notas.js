88430bd5d175e95b31bc448b80f1b44a962e7873
Huan <131563584+zeeywashere@users.noreply.github.com>
2024-09-10 21:01:26 -0300

    Add files via upload

---
+++ Notas.js
@@ -0,0 +1,16 @@
+///////////////// Notas e Pesos /////////////////
+
+
+let n1 = 1.4, n2 = 9.3, n3 = 6.5;
+let p1 = 2, p2 = 3, p3 = 5;
+
+///////////////// Calculo da Média /////////////////
+
+
+let media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
+
+
+///////////////// Mostrar a Média /////////////////
+
+
+console.log('A sua média é:', media);
